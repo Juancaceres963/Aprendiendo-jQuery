6f1b9bb788ed976017831be10609754396b32d69
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-22 10:56:25 -0300

    Creando repo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Signo de dolar "$" es el selector universal de jQuery, es lo mismo a decir jQuery. $ = jQuery
+// Ready es un metodo que significa cuando este listo algo, ejecutara algo 
+// Estos son los dos tipos de selectores donde se debe inyectar el codigo JS
+
+// Selector 1#
+// $(function(){
+//   "use strict"
+// });
+
+//Selector 2#
+$(document).ready(function(){
+    "use strict";
+    console.log("Hola desde jQuery")
+});
+
