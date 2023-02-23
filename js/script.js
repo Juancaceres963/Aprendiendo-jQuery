4240a5931db7cc1ee9ad62d56004a544c4a07998
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-23 09:46:25 -0300

    Show, toggle. Eventos on(), click, mouseenter y mouseleave

--- js/script.js
+++ js/script.js
@@ -36,19 +36,48 @@ $(document).ready(function(){
     // // un display none;
     // $("main article:first").hide();
 
+    // El metodo show() muestra un elemeto que este escondido, es lo contrario a hide()
+    // $("main article:first").show();
+
+    // El metodo toggle() . Este oculta y muestra un elelemto, es la combinacion de hide y de show
+    // $("main article:first").toggle();
+
     //=========== Clorar un elemento =================
 
-    // Clone() Es el metodo que se utiliza para clonar un elemento
-    //Este metodo solo permite clonar una sola copia del elemento
-    var copia = $("main article:last").clone();
-    // append() Es un metodo que incorporara una copia al final de un elemento 
-    $("main").append(copia);
+    // // Clone() Es el metodo que se utiliza para clonar un elemento
+    // //Este metodo solo permite clonar una sola copia del elemento
+    // var copia = $("main article:last").clone();
+    // // append() Es un metodo que incorporara una copia al final de un elemento 
+    // $("main").append(copia);
 
-    // prepend() es un metodo que incorporara una copia al principio de un elemento
-    $("main").prepend(copia);
+    // // prepend() es un metodo que incorporara una copia al principio de un elemento
+    // $("main").prepend(copia);
 
     // Existen dos metodos qu ehacen exactamente lo mismo solo que el orden en que se selecciona es al revez
     //appendTo() Ejemplo: $(copia).appendTo("main"); === $("main").append(copia);
      //prependTo() Ejemplo: $(copia).prependTo("main"); === $("main").prepend(copia);
+
+
+    //Agregando eventos 
+
+    // Para agregar eventos con jQuery se utiliza el metodo on() A este hay que pasarle dos parametros en sus parentesis
+    //1ro es el tipo de evento que sera, en este caso "click", y el 2do la funcion de lo que realizara
+    // $("div.logo img").on("click", function(){
+    //     console.log("Has hecho click en el logo");
+    // })
+    // $("#menu").on("click", function(){
+    //     $("#navegacion").toggle();
+    // })
+        
+    // // mouseenter() y mouseleave() .Estos eventos son parecidos al hover de CSS, cuando estas sobre un elemento
+    // //mouseenter entra en accion, y cuando sales de un elemento mouseleave entra en accion.
+    // $("div.logo img").on("mouseenter", function(){
+    //     console.log("Estas sobre el logo");
+    // })
+    // $("div.logo img").on("mouseleave", function(){
+    //     console.log("Saliste del logo");
+    // })
+
+
 });
 
